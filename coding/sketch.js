e0bd6118023568325f9b58eaa0a0ed045cc3a21f
Sonocool13 <yxia0064@uni.sydney.edu.au>
2025-05-19 17:01:12 +1000

    Built a general framework

--- coding/sketch.js
+++ coding/sketch.js
@@ -1,44 +1,84 @@
-let size =100
-let angleOffset =30
-let bianchang
+let degree=45
+let a=0
+let yuanshiR =100
 function setup() {
 	createCanvas(windowWidth, windowHeight);
-	background('green');
-	bianchang =  sqrt(3)*size
-	let num=Math.floor((width/bianchang)*2+1)
-	print(num)
-	angleMode(DEGREES); 
-	
-		for(let o =0;o<=num;o++){
-	for(let i =0;i<=num;i++){
-		let cx =i*bianchang/2
-		let h = sqrt(3) / 2 * bianchang-size;
-		let cy =100
-		if(i%2 !==0){
-			angleOffset =30,
-				fill('red'),
-				cy=100
-		}else{
-				angleOffset =90,
-						fill('pink'),
-				cy=100-h
+	background(100);
+	  angleMode(DEGREES);
+	//------------------------------------------
+		translate(width/2,height/2)
+	//------------------------------------------
+	// noStroke()
+	for(let j=0;j<3;j++){
+	push()
+	 strokeWeight(1)
+	noFill()
+	circle(0,0,j*yuanshiR*1.6)
+	pop()}
+	//------------------------------------------
+	for(let j=0;j<8;j++){
+			for(let i=0;i<6;i++){
+		let b =random(50)
+	push()
+	 strokeWeight(1)
+
+	let x1 = cos(degree*j- 67.5-i*random(3))*yuanshiR/3+0
+		let y1 = sin(degree*j- 67.5-i*random(3))*yuanshiR/3+0
+		line(0,0,x1*b,y1*b)
+	pop()
 			}
-		
-		beginShape();
-  for (let j = 0; j < 3; j++) {
-    let angle = angleOffset + j * 120; // 每隔120°
-    let x = cx + cos(angle) * size;
-    let y = cy + sin(angle) * size;
-		
-    vertex(x, y);
-}
-  
-  endShape();
-				fill('green')
-		circle(cx, cy, 20);
 	}
+		//------------------------------------------
+	for(let j=0;j<360;j++){
+			for(let i=0;i<6;i++){
+		let c = map(i,0,5,50,25)
+	push()
+	 noStroke()
+		fill(255)
+
+	let x1 = cos(j*3)*yuanshiR/1.3*(4-i/10)+0
+		let y1 = sin(j*3)*yuanshiR/1.3*(4-i/10)+0
+		circle(x1,y1,yuanshiR/c)
+	pop()
+			}
+	}
+	
+//------------------------------------------	
+	drawStar(0,0,50)
+
 }
-}
+
 function draw() {
-	circle(mouseX, mouseY, 20);
+	
+	
 }
+
+
+function drawStar(starX,starY,starSize){
+	fill(255)
+//星星
+for(let j=0;j<3;j++){
+			let r =map(j,0,2,starSize,starSize/3)
+				beginShape()
+	for(let i=0;i<=8;i++){
+if(i==0||i==4||i==8){
+	a=starSize/3
+}else{
+		a=0
+	}
+		let x = cos(degree*i- 90)*(r+a)*3/2+starX
+		let y = sin(degree*i- 90)*(r+a)*3/2+starY
+		
+		let x1 = cos(degree*i- 67.5)*r/3+starX
+		let y1 = sin(degree*i- 67.5)*r/3+starY
+		// line(0,0,x1*width,y1*width)
+		vertex(x,y)
+		vertex(x1,y1)
+		
+		
+	
+	}
+		endShape()}
+}
+
+
